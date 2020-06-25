474fcb6473b762456ef895e10d2fff67dfbbab66
mfacu17 <mfacu98@gmail.com>
2020-06-25 19:03:53 -0300

    Componente recursos arreglado

--- frontend/src/components/recursos/Recursos.js
+++ frontend/src/components/recursos/Recursos.js
@@ -1,11 +1,16 @@
 import React,{Component} from 'react'
-import './Recursos.css/'
+import './Recursos.css'
 
-export class Recursos extends Component{
+export default class Recursos extends Component{
 
     render(){
         return(
-            <h1></h1>
+            <div className="container">
+                <div className="header">
+                   <h2 className="font-poppins text-center my-3">Solicitudes de Recursos</h2>
+                </div>
+                <hr/>
+            </div>
         )
     }
 
